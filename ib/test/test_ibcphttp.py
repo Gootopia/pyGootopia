e4d8c1920e77192a162ca755b2df38ec12c994bf
M K <mattkerschbaum@hotmail.com>
2020-11-22 14:48:12 -0800

    Removed google and TWS api folders as these aren't required for client port API

--- ib/test/test_ibcphttp.py
+++ ib/test/test_ibcphttp.py
@@ -1,21 +1,23 @@
 import unittest
-from pyIB.ib.ibcphttp import ibcphttp
+from ib.ibclientportal import ibcphttp
+
 
 class test_ibcphttp(unittest.TestCase):
     # Make sure base string for client portal is correct
     def test_apiUrlBase(self):
-        self.assertEqual(ibcphttp.apiUrlBase,'https://localhost:5000/v1/portal','Client base url incorrect')
+        self.assertEqual(ibcphttp.apiUrlBase, 'https://localhost:5000/v1/portal', 'Client base url incorrect')
 
     def test_buildEndpointBaseString(self):
         # check with blank string just to make sure the base string format is correct
-        url = ibcphttp.buildEndpointURL(self, ibcphttp.Endpoints.Blank)
-        self.assertEqual(url,ibcphttp.apiUrlBase,'Incorrect base url format')
+        url = ibcphttp.build_endpoint_url(self, ibcphttp.Endpoints.Blank)
+        self.assertEqual(url, ibcphttp.apiUrlBase, 'Incorrect base url format')
 
     def test_statusEndpointString(self):
-        self.assertEqual(ibcphttp.Endpoints.Status.value,'/iserver/auth/status','Status endpoint incorrect')
+        self.assertEqual(ibcphttp.Endpoints.Status.value, '/iserver/auth/status', 'Status endpoint incorrect')
 
     def test_tradesEndpointString(self):
-        self.assertEqual(ibcphttp.Endpoints.Trades.value,'/iserver/account/trades','Trades endpoint incorrect')
+        self.assertEqual(ibcphttp.Endpoints.Trades.value, '/iserver/account/trades', 'Trades endpoint incorrect')
+
 
 if __name__ == '__main__':
     unittest.main()
