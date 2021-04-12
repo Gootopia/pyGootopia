73d9c17a333d2f52e8505f92cee65dfaa93afcca
M K <mattkerschbaum@hotmail.com>
2021-04-12 06:06:56 -0700

    Added sample pytest unit test with passing and failing tests

---
+++ ib/test/clientportal_test.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+class TestClientPortal:
+    def test_something(self):
+        assert True == True
+        assert True == False
+
+
+if __name__ == '__main__':
+    pytest.main()
