d84391e9f259d1b49b6a38e965b899510eebfc2f
M K <mattkerschbaum@hotmail.com>
2021-06-13 19:26:50 -0700

    Updates to configuration code and unit tests

--- lib/configuration/configuration.py
+++ lib/configuration/configuration.py
@@ -1,22 +1,59 @@
-import os
-from configobj import ConfigObj
+from configobj import ConfigObj, Section
+from validate import Validator, ValidateError
+from loguru import logger
+
+
+class ConfigurationInvalidKey(Exception):
+    def __init__(self, key):
+        self.key = key
 
 
 class Configuration(ConfigObj):
-    def __init__(self, infile='config.ini', configspec='config_spec.ini'):
+    """ Wrapper for ConfigObj which allows key indexing with out multi-square brackets """
+    def __init__(self, infile='config.ini', configspec='config_spec.ini', delimeter='/'):
         super().__init__(infile, configspec=configspec, file_error=True)
+        logger.log('DEBUG', f'Configuration: {infile}, Spec: {configspec}')
+        validator = Validator()
+        results = self.validate(validator)
 
-    def get(self, key):
-        """ return parameter using multi level format """
-        if type(key) is not str:
+        if results is not True:
+            raise ValidateError
+
+        if type(delimeter) is str:
+            self.param_delimeter = delimeter
+        else:
+            logger.log('DEBUG', f'Configuration: Non-string delimeter ({delimeter})')
             raise TypeError
 
-        try:
-            val = self[key]
-        except KeyError:
-            val = None
-        finally:
-            return val
+    def get(self, key):
+        """ return parameter using multi level indexing similar to file paths:
+            Example: level1/level2/level3/parameter_name
+        """
+        if type(key) is not str:
+            logger.log('DEBUG', f'Configuration: Non-String ({key}) passed as parameter name')
+            raise TypeError
+
+        levels = key.split(self.param_delimeter)
+        parse_obj = self
+        param_val = None
+
+        for level in levels:
+            try:
+                val = parse_obj[level]
+                if type(val) is Section:
+                    parse_obj = val
+                else:
+                    param_val = val
+            except Exception as e:
+                logger.log('DEBUG', f'EXCEPTION: Unknown {e}')
+
+        if param_val is None:
+            logger.log('DEBUG', f'Configuration: Parameter ({key}) not found and has no default value')
+            bad_param_exception = ConfigurationInvalidKey(key)
+            raise bad_param_exception
+        else:
+            logger.log('DEBUG', f'Configuration: {key}={param_val} ({type(param_val)})')
+            return param_val
 
 
 if __name__ == '__main__':
--- lib/configuration/test/test_configuration.py
+++ lib/configuration/test/test_configuration.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import patch
-from lib.configuration.configuration import Configuration
+from lib.configuration.configuration import Configuration, ConfigurationInvalidKey
+from validate import ValidateError
 
 
 class TestConfiguration:
@@ -31,11 +32,68 @@ class TestConfiguration:
             TestConfiguration.filename = 'config_spec.ini'
             c = Configuration()
 
+    def test_validation_failure(self):
+        """ Catch a validation error """
+        with pytest.raises(ValidateError):
+            # TODO: Probably cleaner to patch the Validation function
+            c = Configuration(configspec='config_spec_bad.ini')
+
     def test_get_not_string(self):
         """ Make sure key is passed as a string """
         with pytest.raises(TypeError):
             c = Configuration()
             val = c.get(0)
+
+    def test_get_parameter_not_found(self):
+        """ Catch when a parameter does not exist (i.e: config_spec does not provide default value)
+            We also want to make sure we get the name back as we can wrap a bunch of gets in a try block
+         """
+        invalid_name = 'bad_parameter_name'
+        with pytest.raises(ConfigurationInvalidKey) as e:
+            c = Configuration()
+            val = c.get(invalid_name)
+            assert val is None
+
+        assert e.value.key == invalid_name
+
+    def test_delimeter_is_string(self):
+        """ Delimeter can only be a character string"""
+        with pytest.raises(TypeError):
+            c = Configuration(delimeter=0)
+
+    def test_get_single_level(self):
+        """ Check lowest level key. Check that we get the same value as when we access using ConfigObj style (dict)"""
+        c = Configuration()
+        val = c.get('level0_param1')
+        assert val == 1234
+        assert val == c['level0_param1']
+
+    def test_get_level1(self):
+        """ Check indented key (delimited). Also check that we get same value as when we access using ConfigObj
+            method. This means the multiple [] of multi-dict access
+        """
+        c = Configuration()
+        val = c.get('level1/param1')
+        assert val == 5678
+        assert val == c['level1']['param1']
+        val = c.get('level1/param2')
+        assert val == 9101112
+        val = c.get('level1/param3')
+        assert val == 'teststring'
+
+    def test_get_multi_level(self):
+        c = Configuration()
+        val = c.get('level1/level2/param1')
+        assert val == 'ABCD'
+
+
+
+
+
+
+
+
+
 
 
 
